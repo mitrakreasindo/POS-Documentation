b5548d96e33ead89904bf776f2382f489725f1db
Hendric <hendric@mitrakreasindo.com>
2017-08-04 09:54:50 +0700

    add version 7

---
+++ Alter-tables-versi07.sql
@@ -0,0 +1,184 @@
+﻿-- Bernaridho. 2017-07-30.
+ALTER TABLE "Step07".Breaks ADD CONSTRAINT PK_Breaks PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Closed_Cashes ADD CONSTRAINT PK_Closed_Cashes PRIMARY KEY (money);
+
+ALTER TABLE "Step07".CSV_Imports ADD CONSTRAINT PK_CSV_Imports PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Consumers ADD CONSTRAINT PK_Consumers PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Drawer_Openings ADD CONSTRAINT PK_Drawer_Openings PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Leaves ADD CONSTRAINT PK_Leaves PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Removed_items ADD CONSTRAINT PK_Removed_items PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Locations ADD CONSTRAINT PK_Locations PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Purchase_orders ADD CONSTRAINT PK_Purchase_orders PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Payments ADD CONSTRAINT PK_Payments PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Persons ADD CONSTRAINT PK_Persons PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Persons ADD CONSTRAINT UK1_Persons UNIQUE (Phone); -- added 2017-07-31
+
+ALTER TABLE "Step07".Persons ADD CONSTRAINT UK2_Persons UNIQUE (E_mail); -- added 2017-07-31
+
+ALTER TABLE public.Merchants ADD CONSTRAINT PK_Merchants PRIMARY KEY (code);
+
+ALTER TABLE public.Merchants ADD CONSTRAINT PK_Merchants UNIQUE (Name);
+
+ALTER TABLE public.Merchants ADD CONSTRAINT UK1_Merchant_categories UNIQUE (Id);
+
+ALTER TABLE public.Merchants ADD CONSTRAINT UK2_Merchant_categories UNIQUE (Category, Subcategory);
+
+--ALTER TABLE "Step07".Places ADD CONSTRAINT PK_Places PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Product_Categories ADD CONSTRAINT PK_Product_Categories PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Product_coms ADD CONSTRAINT PK_Product_coms PRIMARY KEY (id, Product_id, Product2_id); -- previously (id, Product, Product2)
+
+ALTER TABLE "Step07".Product_kits ADD CONSTRAINT PK_Product_kits PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Products ADD CONSTRAINT PK_Products PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Promotions ADD CONSTRAINT PK_Promotions PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Receipts ADD CONSTRAINT PK_Receipts PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Roles ADD CONSTRAINT PK_Roles PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Shift_breaks ADD CONSTRAINT PK_Shift_breaks PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Shifts ADD CONSTRAINT PK_Shifts PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Stock_Changes ADD CONSTRAINT PK_Stock_Changes PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Stock_Diaries ADD CONSTRAINT PK_Stock_Diaries PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Stock_Levels ADD CONSTRAINT PK_Stock_Levels PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Tax_Categories ADD CONSTRAINT PK_Tax_Categories PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Taxes ADD CONSTRAINT PK_Taxes PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Tax_Items ADD CONSTRAINT PK_Tax_Items PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Third_Parties ADD CONSTRAINT PK_Third_Parties PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Sales ADD CONSTRAINT PK_Sales PRIMARY KEY (id);
+
+ALTER TABLE "Step07".Sales_items ADD CONSTRAINT PK_Sales_items PRIMARY KEY (Sales_id, line); -- previously (Ticket, line)
+
+ALTER TABLE "Step07".Vouchers ADD CONSTRAINT PK_Vouchers PRIMARY KEY (Name); -- previously voucher
+
+-- Comment ==================================================================================
+
+ALTER TABLE "Step07".Product_Categories
+ ADD CONSTRAINT FK_Product_Categories 
+  FOREIGN KEY (Parent_Id) -- previously ParentId
+  REFERENCES "Step07".Product_Categories (id)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION; -- changed
+
+ALTER TABLE "Step07".Leaves
+    ADD CONSTRAINT FK_Leaves FOREIGN KEY (Person_Id) REFERENCES "Step07".Persons (id); -- changed
+
+ALTER TABLE public.Merchants -- added 2017-07-31
+    ADD CONSTRAINT FK_Merchants FOREIGN KEY (Category_Id) REFERENCES public.Merchant_categories (id);
+    
+ALTER TABLE "Step07".Payments
+    ADD CONSTRAINT FK_payments FOREIGN KEY (Receipt_id) REFERENCES "Step07".Receipts (id); -- changed
+
+ALTER TABLE "Step07".Persons
+    ADD CONSTRAINT FK_Persons FOREIGN KEY (role_id) REFERENCES "Step07".Roles (id); -- changed
+
+ALTER TABLE "Step07".Products -- changed, renamed, reordered
+    ADD CONSTRAINT FK1_Products FOREIGN KEY (Category_id) REFERENCES "Step07".Product_Categories (id);
+
+ALTER TABLE "Step07".Products -- changed, renamed, reordered
+ ADD CONSTRAINT FK2_Products 
+  FOREIGN KEY (PackProduct_id) 
+  REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Products -- changed, renamed
+    ADD CONSTRAINT FK3_Products FOREIGN KEY (Promotion_Id) REFERENCES "Step07".Promotions (id);
+
+ALTER TABLE "Step07".Products -- changed
+    ADD CONSTRAINT FK4_Products FOREIGN KEY (TaxCategory_id) REFERENCES "Step07".Tax_Categories (id);
+
+ALTER TABLE "Step07".Product_coms -- changed
+    ADD CONSTRAINT FK1_Product_coms FOREIGN KEY (Product_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Product_coms -- changed
+    ADD CONSTRAINT FK2_Product_coms FOREIGN KEY (Product2_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Product_kits -- changed
+    ADD CONSTRAINT FK1_Product_kits FOREIGN KEY (Product_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Product_kits -- unchanged
+    ADD CONSTRAINT FK2_Product_kits FOREIGN KEY (Product_kit) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Receipts -- changed references
+    ADD CONSTRAINT FK_Receipts FOREIGN KEY (id) REFERENCES "Step07".Sales (id);
+
+ALTER TABLE "Step07".Shift_breaks -- changed
+    ADD CONSTRAINT FK1_Shift_breaks FOREIGN KEY (Break_Id) REFERENCES "Step07".Breaks (id);
+
+ALTER TABLE "Step07".Shift_breaks -- changed
+    ADD CONSTRAINT FK2_Shift_breaks FOREIGN KEY (Shift_Id) REFERENCES "Step07".Shifts (id);
+
+ALTER TABLE "Step07".Stock_Changes -- changed
+    ADD CONSTRAINT FK1_Stock_Changes FOREIGN KEY (Location_id) REFERENCES "Step07".Locations (id);
+
+ALTER TABLE "Step07".Stock_Changes -- changed
+    ADD CONSTRAINT FK2_Stock_Changes FOREIGN KEY (Product_Id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Stock_Currents -- changed, renamed
+    ADD CONSTRAINT FK1_Stock_Currents FOREIGN KEY (Location_id) REFERENCES "Step07".Locations (id);
+
+ALTER TABLE "Step07".Stock_Currents -- changed
+    ADD CONSTRAINT FK2_Stock_Currents FOREIGN KEY (Product_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Stock_Diaries -- changed, renamed
+    ADD CONSTRAINT FK1_Stock_Diaries FOREIGN KEY (Location_id) REFERENCES "Step07".Locations (id);
+
+ALTER TABLE "Step07".Stock_Diaries -- changed
+    ADD CONSTRAINT FK2_Stock_Diaries FOREIGN KEY (Product_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Stock_Levels -- changed
+    ADD CONSTRAINT FK1_Stock_Levels FOREIGN KEY (Location_id) REFERENCES "Step07".Locations (id);
+
+ALTER TABLE "Step07".Stock_Levels -- changed
+    ADD CONSTRAINT FK2_Stock_Levels FOREIGN KEY (Product_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Taxes -- changed
+    ADD CONSTRAINT FK1_Taxes FOREIGN KEY (Category_id) REFERENCES "Step07".Tax_Categories (id);
+
+ALTER TABLE "Step07".Taxes -- changed, renamed
+    ADD CONSTRAINT FK2_Taxes FOREIGN KEY (Parent_Id) REFERENCES "Step07".Taxes (id);
+
+ALTER TABLE "Step07".Tax_Items -- changed
+    ADD CONSTRAINT FK1_Tax_Items FOREIGN KEY (Receipt_id) REFERENCES "Step07".Receipts (id);
+
+ALTER TABLE "Step07".Tax_Items -- changed
+    ADD CONSTRAINT FK2_Tax_Items FOREIGN KEY (Tax_Id) REFERENCES "Step07".Taxes (id);
+
+ALTER TABLE "Step07".Sales_items -- changed, renamed
+    ADD CONSTRAINT FK1_Sales_items FOREIGN KEY (Product_id) REFERENCES "Step07".Products (id);
+
+ALTER TABLE "Step07".Sales_items -- changed, renamed
+    ADD CONSTRAINT FK2_Sales_items FOREIGN KEY (Tax_Id) REFERENCES "Step07".Taxes (id);
+
+ALTER TABLE "Step07".Sales_items -- changed, renamed
+    ADD CONSTRAINT FK3_Sales_items FOREIGN KEY (Sales_id) REFERENCES "Step07".Sales (id);
+
+ALTER TABLE "Step07".Sales -- changed
+    ADD CONSTRAINT FK1_Sales FOREIGN KEY (Consumer_id) REFERENCES "Step07".Consumers (id);
+
+ALTER TABLE "Step07".Sales -- changed
+    ADD CONSTRAINT FK2_Sales FOREIGN KEY (Person_id) REFERENCES "Step07".Persons (id);
+
+--ALTER TABLE "Step07".Sales
+--    ADD CONSTRAINT FK3_Sales FOREIGN KEY  (id) REFERENCES "Step07".Receipts (id);
---
+++ Create-tables-versi07.sql
@@ -0,0 +1,412 @@
+﻿-- In schema "Step07"; several columns are renamed
+-- Some columns are merged.
+-- Bernaridho. 2017-07-30.
+
+--CREATE TYPE timestamp FROM DateTime ;
+
+CREATE TABLE "Step07".Breaks 
+(
+  Id varchar(15) NOT NULL,
+  Is_visible boolean default true NOT NULL, -- previously Visible
+  Name varchar(25), -- NOT NULL
+  Notes varchar(50)
+);
+
+
+CREATE TABLE "Step07".Closed_Cashes 
+(
+  Host varchar(50), -- NOT NULL
+  Money varchar(20) NOT NULL,
+  Sales_count integer DEFAULT 0 NOT NULL, -- previously NoSales
+  Start_timestamp timestamp default current_timestamp NOT NULL, -- previously DateStart
+  Stop_timestamp timestamp -- previously DateEnd
+);
+
+
+CREATE TABLE "Step07".Consumers 
+(
+  Address varchar(40),
+  Address2 varchar(50),
+  Card varchar(20),
+  City varchar(25),
+  Country varchar(25),
+  Start_Date date default current_date, -- previously CurDate
+  CurDebt float(52) DEFAULT 0,
+  DOB Date, -- previously timestamp
+  Discount float(23) DEFAULT 0, -- previously float(52)
+  E_mail varchar(40), 
+  Fax varchar(20),
+  Id varchar(20) NOT NULL, -- fill value of this column with the Name value
+  Image ByteA,
+  Is_visible boolean default true,
+  Key4Searching varchar(255), 
+  MaxDebt float(52) DEFAULT 0, -- NOT NULL
+  Name varchar(40) DEFAULT ' ' NOT NULL, 
+  Notes varchar(50),
+  Phone varchar(20),
+  Phone2 varchar(20),
+  Region varchar(20),
+  TaxCategory_Id varchar(25), 
+  Tax_Id varchar(25), 
+  Zipcode varchar(255)
+);
+
+
+CREATE TABLE "Step07".CSV_Imports 
+(
+  Category varchar(30),
+  Code varchar(20),
+  CSV_Error varchar(255), 
+  Id varchar(30) NOT NULL,
+  Name varchar(30),
+  Previous_Buy float(52), 
+  Previous_Sell float(52), 
+  Price_Buy float(52), 
+  Price_Sell float(52), 
+  Reference varchar(30),
+  Record_Number varchar(20) 
+);
+
+
+CREATE TABLE "Step07".Drawer_Openings 
+(
+  Id varchar(255) NOT NULL,
+  Name varchar(255),
+  Sales_Id varchar(40),
+  "Timestamp" timestamp default current_timestamp -- previously OpenDate
+);
+
+
+CREATE TABLE "Step07".Leaves 
+(
+  Id varchar(20) NOT NULL,
+  Name varchar(50), 
+  Notes varchar(50),
+  Person_Id varchar(20) NOT NULL, 
+  Stop_date date NOT NULL, -- previously EndDate timestamp
+  Start_Date date NOT NULL -- previously StartDate timestamp
+);
+
+
+CREATE TABLE "Step07".Locations 
+(
+  Address varchar(40),
+  Id varchar(40) NOT NULL,
+  Name varchar(30) -- NOT NULL
+);
+
+
+CREATE TABLE public.Merchants 
+(
+  Address varchar(40) NOT NULL, -- previously text
+  Category_id SmallInt NOT NULL,
+  Code varchar(4) NOT NULL,
+  E_mail varchar(40) NOT NULL,
+  Name varchar(40) NOT NULL,
+  NPWP varchar(25), -- Nomor Pokok Wajib Pajak
+  Phone varchar(20) NOT NULL
+);
+
+
+CREATE TABLE public.Merchant_categories
+(
+  Id SmallSerial,
+  Category varchar(50),
+  Subcategory varchar(50)
+);
+
+
+CREATE TABLE "Step07".Payments 
+(
+  Card varchar(20), -- previously CardName
+  Id varchar(20) NOT NULL,
+  Notes varchar(50),
+  Payment varchar(50) NOT NULL,
+  Receipt_id varchar(40) NOT NULL, 
+  Returned_Msg ByteA, 
+  Tendered float(52) DEFAULT 0 NOT NULL,
+  Total float(52) NOT NULL,
+  Transaction_Id varchar(20) 
+);
+
+
+CREATE TABLE "Step07".Persons 
+(
+  App_Password varchar(40), 
+  Card varchar(20),
+  E_mail varchar(40) NOT NULL,
+  Id varchar(20) NOT NULL,
+  Image ByteA,
+  Is_visible boolean,
+  Name varchar(40),
+  Phone varchar(20) NOT NULL, -- added on 2017-07-31
+  Role_id varchar(20) NOT NULL 
+);
+
+
+CREATE TABLE "Step07".Product_Categories 
+(
+  Colour varchar(20),
+  Id varchar(30) NOT NULL,
+  Image ByteA,
+  Is_name_in_catalog boolean default true NOT NULL, 
+  Name varchar(30), 
+  Order_in_catalog integer,  
+  Parent_Id varchar(30), 
+  Text_Tip varchar(50) DEFAULT NULL 
+);
+
+
+CREATE TABLE "Step07".Product_coms 
+(
+  Id varchar(25) NOT NULL,
+  Product_id varchar(25) NOT NULL, -- previously Product 
+  Product2_id varchar(25) NOT NULL -- previously Product2
+);
+
+
+CREATE TABLE "Step07".Product_kits 
+(
+  Id varchar(25) NOT NULL,
+  Product_id varchar(25) NOT NULL, -- previously Product
+  Product_kit varchar(25) NOT NULL,
+  Quantity float(23) NOT NULL -- previously float(52)
+);
+
+
+CREATE TABLE "Step07".Products 
+(
+  Alias varchar(20),
+  Category_id varchar(30) NOT NULL, 
+  Code varchar(20), -- NOT NULL
+  CodeType varchar(20),
+  Discounted varchar(5) DEFAULT 'no',
+  Display varchar(25),
+  Id varchar(25) NOT NULL,
+  Image ByteA,
+  Is_All_Products boolean default false, 
+  Is_Always_Available boolean default false NOT NULL, 
+  Is_Com boolean default false,  
+  Is_Discountable boolean default true NOT NULL, 
+  Is_in_Catalog boolean, -- previously Is_catalog
+  Is_Managed_in_Stock boolean default true, 
+  Is_Pack boolean default false, 
+  Is_Scale boolean default false, 
+  Is_sent boolean default false NOT NULL,
+  Is_Service boolean default false, 
+  Is_Volume_Price boolean default false, 
+  Is_warranted boolean default false NOT NULL, 
+  Name varchar(30) NOT NULL,
+  Order_in_catalog SmallInt, 
+  Pack_Quantity float(23), -- previously float(52) 
+  PackProduct_id varchar(25), 
+  Price_Buy float(52) DEFAULT 0 NOT NULL, 
+  Price_Sell float(52) DEFAULT 0 NOT NULL, 
+  Promotion_Id varchar(20), 
+  Reference varchar(20), -- NOT NULL
+  Stock_Cost float(23), -- previously float(52)
+  Stock_Units float(23) DEFAULT 0 NOT NULL,  -- previously float(52)
+  Stock_Volume float(23),  -- previously float(52)
+  TaxCategory_id varchar(25), -- NOT NULL
+  Text_Tip varchar(50)
+);
+
+
+CREATE TABLE "Step07".Promotions 
+(
+  Criteria ByteA,
+  Id varchar(20) NOT NULL,
+  Is_All_Products boolean default false, 
+  Is_Enabled boolean default true, 
+  Name varchar(50), -- NOT NULL
+  Script ByteA NOT NULL
+);
+
+
+CREATE TABLE "Step07".Purchase_orders 
+(
+  Auxiliary Integer,
+  Details varchar(100),
+  Id varchar(50) NOT NULL,
+  Notes varchar(50),
+  Quantity Integer, 
+  Sales_Id varchar(40),
+  Sequence integer DEFAULT 0 NOT NULL,
+  Timestamp timestamp default current_timestamp -- Plan B: Order_Time
+);
+
+
+CREATE TABLE "Step07".Receipts 
+(
+  Id varchar(40) NOT NULL,
+  Money varchar(20) NOT NULL,
+  Person_id varchar(20),
+  Timestamp timestamp  default current_timestamp NOT NULL -- previously DateNew
+);
+
+
+CREATE TABLE "Step07".Removed_items 
+(
+  Id varchar(20) NOT NULL,
+  Product_Id varchar(25), 
+  Product_Name varchar(30), 
+  Date date default current_date NOT NULL, -- previously of type timestamp
+  Sales_Id varchar(40), 
+  Units float(23) NOT NULL -- previously float(52)
+);
+
+
+CREATE TABLE "Step07".Roles 
+(
+  Id varchar(20) NOT NULL,
+  Name varchar(20), -- NOT NULL
+  Permissions ByteA,
+  Rights_Level integer DEFAULT 4 NOT NULL 
+);
+
+
+CREATE TABLE "Step07".Sales 
+(
+  Consumer_id varchar(20), 
+  Id varchar(40) NOT NULL,
+  Person_id varchar(20) NOT NULL, 
+  Sales_Id varchar(40) NOT NULL, 
+  Sales_Type SmallInt NOT NULL,
+  Status SmallInt DEFAULT 0 NOT NULL
+);
+
+
+CREATE TABLE "Step07".Sales_items 
+(
+  Line SmallInt NOT NULL,
+  Price float(52) NOT NULL,
+  Product_id varchar(25), 
+  Refunded_Quantity float(23) DEFAULT 0, -- previously float(52)
+  Sales_id varchar(40) NOT NULL, 
+  Tax_Id varchar(25) NOT NULL, 
+  units float(23) NOT NULL -- previously float(52)
+);
+
+
+CREATE TABLE "Step07".Shift_Breaks 
+(
+  Break_Id varchar(15) NOT NULL, 
+  Id varchar(20) NOT NULL,
+  Shift_Id varchar(20) NOT NULL, 
+  Start_timestamp timestamp NOT NULL, -- previously StartTime
+  Stop_timestamp timestamp -- previously EndTime
+);
+
+
+CREATE TABLE "Step07".Shifts 
+(
+  Id varchar(20) NOT NULL,
+  Person_Id varchar(20), 
+  Start_time time NOT NULL, -- previously StartShift timestamp
+  Stop_time time -- previously EndShift timestamp
+);
+
+
+CREATE TABLE "Step07".Stock_Changes 
+(
+  BLOb_Value ByteA, 
+  Changes_processed integer,
+  Change_Type integer, 
+  Display varchar(20),
+  Columns varchar(50), 
+  Id varchar(20) NOT NULL,
+  Location_id varchar(40), 
+  Product_Id varchar(25), 
+  Text_Value varchar(50), 
+  Upload_Timestamp timestamp default current_timestamp,  -- previously Upload_Time
+  UserName varchar(40)
+);
+
+
+CREATE TABLE "Step07".Stock_Currents 
+(
+  Location_id varchar(40), -- NOT NULL
+  Product_id varchar(25) NOT NULL, 
+  units float(23) DEFAULT 0 NOT NULL -- previously float(52)
+);
+
+
+CREATE TABLE "Step07".Stock_Diaries 
+(
+  App_User varchar(40), 
+  Date date default current_date NOT NULL, -- previously DateNew timestamp
+  Id varchar(20) NOT NULL,
+  Location_id varchar(40), -- NOT NULL
+  Price float(52) NOT NULL,
+  Product_id varchar(25) NOT NULL, 
+  reason SmallInt NOT NULL,
+  units float(23) NOT NULL -- previously float(52)
+);
+
+
+CREATE TABLE "Step07".Stock_Levels 
+(
+  Id varchar(25) NOT NULL,
+  Location_id varchar(40), -- NOT NULL
+  Maximum float(23) NOT NULL, -- previously float(52)
+  Product_id varchar(25) NOT NULL, 
+  Security SmallInt NOT NULL -- previously float(52)
+);
+
+
+CREATE TABLE "Step07".Tax_Categories 
+(
+  Id varchar(25) NOT NULL,
+  Name varchar(30) -- NOT NULL
+);
+
+
+CREATE TABLE "Step07".Tax_Items 
+(
+  Amount float(52) DEFAULT 0 NOT NULL,
+  Base float(52) DEFAULT 0 NOT NULL,
+  Id varchar(25) NOT NULL,
+  Receipt_id varchar(40) NOT NULL, 
+  Tax_Id varchar(25) NOT NULL 
+);
+
+
+CREATE TABLE "Step07".Taxes 
+(
+  Category_id varchar(25) NOT NULL, 
+  Consumer_Category_id varchar(25), -- unused at the moment
+  Id varchar(25) NOT NULL,
+  Is_Cascading_Rate boolean default false NOT NULL, 
+  Name varchar(30), -- fill with Id value
+  Parent_Id varchar(25), 
+  Rate float(23) DEFAULT 0 NOT NULL, -- previously float(52)
+  Rate_Order integer 
+);
+
+
+CREATE TABLE "Step07".Third_Parties 
+(
+  Address varchar(40),
+  CIF varchar(255) NOT NULL, -- for filename
+  Contact_Com varchar(40), 
+  Contact_Fact varchar(100), 
+  E_mail varchar(40), 
+  Fax varchar(20), 
+  Id varchar(25) NOT NULL,
+  Mobile_Phone varchar(20), 
+  Name varchar(30), -- fill with Id value
+  Notes varchar(50),
+  Payment_Rule varchar(255), 
+  Phone varchar(255), 
+  website varchar(255) 
+);
+
+
+CREATE TABLE "Step07".Vouchers 
+(
+  Name varchar(50) NOT NULL, 
+  Redeemed_Date date, -- previously timestamp
+  Redeemed_Sales_Id varchar(50), 
+  Sold_Date date ,  -- previously timestamp
+  Sold_Sales_Id varchar(50) 
+);
